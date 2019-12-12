6c6bfce696f6259bd65b714c12dcf2294b06a598
drc1313 <dcote123123@gmail.com>
2019-12-11 23:42:45 -0500

    new model and brush size change

--- app.py
+++ app.py
@@ -19,14 +19,19 @@ def predict(image):
     image = np.array(image)
     image = image.reshape(784)
     image = np.expand_dims(image, axis=0)
-    print(image[0][0])
+    print(len(image[0]))
     for k in range(len(image[0])):
         if image[0][k]==255:
             image[0][k]=0
         else:
             if image[0][k]>0:
                 image[0][k]=1
-    print(image)
+
+
+    for k in range(len(image[0])):
+        if k%14==0:
+            print(image[0][k-14:k])
+    # print(image)
     preds = model.predict(image)
 
     return np.where(preds == preds.max())[1][0]
